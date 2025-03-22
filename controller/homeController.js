a33f4ff344af77eaf08c870310328fb6ff2bf010
Vinicius <vmeedeiros@gmail.com>
2025-03-22 00:38:46 -0300

    ajusta Layout

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,34 +1,34 @@
 class HomeController {
   home(req, res) {
-    res.render('home.ejs');
+    res.render('home.ejs', { layout: './layouts/layout.ejs' });
   }
 
   contato(req, res) {
-    res.render('contato.ejs');
+    res.render('contato.ejs', { layout: './layouts/layout.ejs' });
   }
 
   sobre(req, res) {
-    res.render('sobre.ejs');
+    res.render('sobre.ejs', { layout: './layouts/layout.ejs' });
   }
 
   ensino(req, res) {
-    res.render('ensino.ejs');
+    res.render('ensino.ejs', { layout: './layouts/layout.ejs' });
   }
 
   professores(req, res) {
-    res.render('professores.ejs');
+    res.render('professores.ejs', { layout: './layouts/layout.ejs' });
   }
 
   galeria(req, res) {
-    res.render('galeria.ejs');
+    res.render('galeria.ejs', { layout: './layouts/layout.ejs' });
   }
 
   jornal(req, res) {
-    res.render('jornal.ejs');
+    res.render('jornal.ejs', { layout: './layouts/layout.ejs' });
   }
 
   noticias(req, res) {
-    res.render('noticias.ejs');
+    res.render('noticias.ejs', { layout: './layouts/layout.ejs' });
   }
 }
 
--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require('express');
 const expressEjsLayout = require('express-ejs-layouts');
 const rotaHome = require('./routes/rotaHome');
 const rotaLogin = require('./routes/rotaLogin');
+const rotaProfessor = require('./routes/rotaProfessor');
 const app = express();
 const porta = 5000;
 
 app.set('views engine', 'ejs');
 app.use(express.static('public'));
-app.set('layout', './layout.ejs');
+app.set('layout', 'layouts/layout');
 app.use(expressEjsLayout);
 app.use('/', rotaHome);
 app.use('/login', rotaLogin);
+app.use('/seeds/professor', rotaProfessor);
+// app.use('/seeds/aluno', rotaAluno);   - - - Falta desenvolver - - -
 
 app.listen(porta, function () {
   console.log(`Servidor em execução na porta: ${porta}`);

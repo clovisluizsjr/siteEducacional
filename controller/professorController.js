444f3062f3729b9b1b7bfba16f0e3f33aa5fe80d
vinicius <vmeedeiros@gmail.com>
2025-03-22 13:48:54 -0300

    Adiciona novas rotas a professor

--- controller/professorController.js
+++ controller/professorController.js
@@ -3,8 +3,27 @@ class ProfessorController {
     res.render('seeds/professor.ejs', {
       layout: './layouts/layoutSeeds.ejs',
       rota: 'professor',
-      imgUser: '../img/team-4.jpg',
-      nomeUser: 'Professor Pandur',
+      imgUser: '/img/team-4.jpg', // Essas informações viram do banco
+      nomeUser: 'Professor Pandur', // Essas informações viram do banco
+    });
+  }
+
+  async listarAlunos(req, res) {
+    res.render('seeds/alunos.ejs', {
+      layout: './layouts/layoutSeeds.ejs',
+      rota: 'professor',
+      imgUser: '/img/team-4.jpg', // Essas informações viram do banco
+      nomeUser: 'Professor Pandur', // Essas informações viram do banco
+    });
+  }
+
+  async listarDisciplinas(req, res) {
+    res.render('seeds/disciplinas.ejs', {
+      layout: './layouts/layoutSeeds.ejs',
+      rota: 'professor',
+      imgUser: '/img/team-4.jpg', // Essas informações viram do banco
+      nomeUser: 'Professor Pandur', // Essas informações viram do banco
+      // Terá uma chave aqui com as disciplinas que serão renderizadas na tabela
     });
   }
 }
--- routes/rotaProfessor.js
+++ routes/rotaProfessor.js
@@ -5,5 +5,7 @@ const router = expess.Router();
 
 let ctrl = new ProfessorController();
 router.get('/', ctrl.homeSeed);
+router.get('/alunos', ctrl.listarAlunos);
+router.get('/disciplinas', ctrl.listarDisciplinas);
 
 module.exports = router;

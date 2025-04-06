13735734ca403e9748aab058f660441231300638
MariaLedier <mariaclara.ledierdelp@gmail.com>
2025-04-06 17:55:05 -0300

    alteracoes

--- models/atividadeProfessorModel.js
+++ models/atividadeProfessorModel.js
@@ -21,21 +21,7 @@ class AtividadeProfessorModel {
 
   get disciplina_id() { return this.#disciplina_id } set disciplina_id(value) { this.#disciplina_id = value }
 
-  get serie_id() { return this.#serie_id } set serie_id(value) { this.#serie_id = value }
-
-  constructor(atividadeProf_idProf, atividadeProf_tituloProf, atividadeProf_descricaoProf, atividadeProf_notaProf, atividadeProf_prazoProf, disciplina_id, serie_id) {
-    this.atividadeProf_idProf = atividadeProf_idProf;
-    this.atividadeProf_tituloProf = atividadeProf_tituloProf;
-    this.atividadeProf_descricaoProf = atividadeProf_descricaoProf;
-    this.atividadeProf_notaProf = atividadeProf_notaProf;
-    this.atividadeProf_prazoProf = atividadeProf_prazoProf;
-    this.disciplina_id = disciplina_id;
-    this.serie_id = serie_id;
-  }
-
-  async listarAtividadesPor(disciplinaId, serieId){
-    let sql = "SELECT * FROM AtividadeProfessor WHERE disciplina_id = ? AND serie_id = ?"; 
-    let valores = [disciplinaId, serieId];
+  get serie_id() { ret[disciplinaId, serieId];
     let banco = new Database();
     let lista =[];
     let rows = await banco.ExecutaComando(sql, valores);
@@ -70,4 +56,18 @@ class AtividadeProfessorModel {
 
 }
 
-module.exports = AtividadeProfessorModel;
+module.exports = AtividadeProfessorModel;urn this.#serie_id } set serie_id(value) { this.#serie_id = value }
+
+  constructor(atividadeProf_idProf, atividadeProf_tituloProf, atividadeProf_descricaoProf, atividadeProf_notaProf, atividadeProf_prazoProf, disciplina_id, serie_id) {
+    this.atividadeProf_idProf = atividadeProf_idProf;
+    this.atividadeProf_tituloProf = atividadeProf_tituloProf;
+    this.atividadeProf_descricaoProf = atividadeProf_descricaoProf;
+    this.atividadeProf_notaProf = atividadeProf_notaProf;
+    this.atividadeProf_prazoProf = atividadeProf_prazoProf;
+    this.disciplina_id = disciplina_id;
+    this.serie_id = serie_id;
+  }
+
+  async listarAtividadesPor(disciplinaId, serieId){
+    let sql = "SELECT * FROM AtividadeProfessor WHERE disciplina_id = ? AND serie_id = ?"; 
+    let valores = 

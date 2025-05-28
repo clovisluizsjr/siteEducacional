8c92f90c09650cec23ad58954c856a3b7b2fb7a1
Vinicius <vmeedeiros@gmail.com>
2025-05-28 20:32:52 -0300

    ajusta insert de ItensQuadro

--- models/itensQuadroNotas.js
+++ models/itensQuadroNotas.js
@@ -1,101 +1,107 @@
 const Database = require('../utils/database');
 
 class ItensQuadroNotasModel {
-    #id;
-    #quadro;
-    #atividade_id;
-    #descricao;
-    #peso;
-    #tipo;
+  #id;
+  #quadro;
+  #atividade_id;
+  #descricao;
+  #peso;
+  #tipo;
 
-    get id() { return this.#id }
-    set id(value) { this.#id = value }
+  get id() {
+    return this.#id;
+  }
+  set id(value) {
+    this.#id = value;
+  }
 
-    get quadro() { return this.#quadro }
-    set quadro(value) { this.#quadro = value }
+  get quadro() {
+    return this.#quadro;
+  }
+  set quadro(value) {
+    this.#quadro = value;
+  }
 
-    get atividade_id() { return this.#atividade_id }
-    set atividade_id(value) { this.#atividade_id = value }
+  get atividade_id() {
+    return this.#atividade_id;
+  }
+  set atividade_id(value) {
+    this.#atividade_id = value;
+  }
 
-    get descricao() { return this.#descricao }
-    set descricao(value) { this.#descricao = value }
+  get descricao() {
+    return this.#descricao;
+  }
+  set descricao(value) {
+    this.#descricao = value;
+  }
 
-    get peso() { return this.#peso }
-    set peso(value) { this.#peso = value }
+  get peso() {
+    return this.#peso;
+  }
+  set peso(value) {
+    this.#peso = value;
+  }
 
-    get tipo() { return this.#tipo }
-    set tipo(value) { this.#tipo = value }
+  get tipo() {
+    return this.#tipo;
+  }
+  set tipo(value) {
+    this.#tipo = value;
+  }
 
-    constructor(id, quadro, atividade_id, descricao, peso, tipo) {
-        this.#id = id;
-        this.#quadro = quadro;
-        this.#atividade_id = atividade_id;
-        this.#descricao = descricao;
-        this.#peso = peso;
-        this.#tipo = tipo;
+  constructor(id, quadro, atividade_id, descricao, peso, tipo) {
+    this.#id = id;
+    this.#quadro = quadro;
+    this.#atividade_id = atividade_id;
+    this.#descricao = descricao;
+    this.#peso = peso;
+    this.#tipo = tipo;
+  }
+
+  async gravarItem() {
+    const banco = new Database();
+    const sql = `
+        INSERT INTO ItensQuadroNotas (quadro_id, atividade_id, descricao, peso, tipo)
+        VALUES (?, ?, ?, ?, ?)
+        ON DUPLICATE KEY UPDATE
+            descricao = VALUES(descricao),
+            peso = VALUES(peso),
+            tipo = VALUES(tipo)
+    `;
+    const valores = [
+      this.#quadro,
+      this.#atividade_id,
+      this.#descricao,
+      this.#peso,
+      this.#tipo,
+    ];
+    return await banco.ExecutaComando(sql, valores);
+  }
+
+  async listarPorQuadro(quadroId) {
+    const banco = new Database();
+    const sql = `SELECT * FROM ItensQuadroNotas WHERE quadro_id = ?`;
+    const valores = [quadroId];
+    const rows = await banco.ExecutaComando(sql, valores);
+
+    const lista = [];
+    for (let i = 0; i < rows.length; i++) {
+      lista.push(
+        new ItensQuadroNotasModel(
+          rows[i]['id'],
+          rows[i]['quadro_id'],
+          rows[i]['atividade_id'],
+          rows[i]['descricao'],
+          rows[i]['peso'],
+          rows[i]['tipo']
+        )
+      );
     }
+    return lista;
+  }
 
- 
-
-    async gravarItem() {
-        const banco = new Database();
-        if (!this.#id || this.#id === 0) {
-            const sql = `
-                                                INSERT INTO ItensQuadroNotas (quadro_id, atividade_id, descricao, peso, tipo)
-                                                VALUES (?, ?, ?, ?, ?)
-                                            `;
-            const valores = [
-                this.#quadro,
-                this.#atividade_id,
-                this.#descricao,
-                this.#peso,
-                this.#tipo
-            ];
-            const resultado = await banco.ExecutaComando(sql, valores);
-            return resultado;
-        } else {
-            const sql = `
-                                                UPDATE ItensQuadroNotas
-                                                SET quadro_id = ?, atividade_id = ?, descricao = ?, peso = ?, tipo = ?
-                                                WHERE id = ?
-                                            `;
-            const valores = [
-                this.#quadro,
-                this.#atividade_id,
-                this.#descricao,
-                this.#peso,
-                this.#tipo,
-                this.#id
-            ];
-            const resultado = await banco.ExecutaComando(sql, valores);
-            return resultado;
-        }
-    }
-
-   
-
-    async listarPorQuadro(quadroId) {
-        const banco = new Database();
-        const sql = `SELECT * FROM ItensQuadroNotas WHERE quadro_id = ?`;
-        const valores = [quadroId];
-        const rows = await banco.ExecutaComando(sql, valores);
-
-        const lista = [];
-        for (let i = 0; i < rows.length; i++) {
-            lista.push(new ItensQuadroNotasModel(
-                rows[i]['id'],
-                rows[i]['quadro_id'],
-                rows[i]['atividade_id'],
-                rows[i]['descricao'],
-                rows[i]['peso'],
-                rows[i]['tipo']
-            ));
-        }
-        return lista;
-    }
-
-
-    async getPesosPorDisciplina(disciplinaId) {
+  async getPesosPorDisciplina(disciplinaId) {
     let sql = `
     SELECT iq.atividade_id, iq.peso
   FROM ItensQuadroNotas iq

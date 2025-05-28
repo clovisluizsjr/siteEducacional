43af235a88928aa3e23addc53faf8d2cad90c4ec
Vinicius <vmeedeiros@gmail.com>
2025-05-28 20:38:08 -0300

    ajusta insert de ItensQuadro

--- public/js/seeds/professor/validacaoPesos.js
+++ public/js/seeds/professor/validacaoPesos.js
@@ -1,6 +1,7 @@
 function validarSomaPesos() {
   let inputs = document.querySelectorAll('input[name^="atividades"][name$="[peso]"]');
   let soma = 0;
+  let btnSalvar = document.querySelector('.btnSalvarPeso');
 
   inputs.forEach(function(input) {
     let valor = Number(input.value);
@@ -10,12 +11,14 @@ function validarSomaPesos() {
   });
 
   let msg = document.getElementById('mensagemSomaPesos');
-  if (soma !== 10) {
-    msg.textContent = "A soma dos pesos tem que dar 10. Soma atual: " + soma;
+  if (soma !== 10 || inputs[0].value <= 0 || inputs[1].value <= 0) {
+    msg.textContent = `A soma dos pesos deve ser 10 e os valores precisam ser positivos. Soma atual: ${soma}`;
     msg.style.color = "red";
+    btnSalvar.disabled = true
   } else {
     msg.textContent = "Soma certa: " + soma;
     msg.style.color = "green";
+    btnSalvar.disabled = false;
   }
 }
 
